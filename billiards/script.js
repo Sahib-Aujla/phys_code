c52e017d9ea249b20db1a30f530374b3bd409b30
Sahib.Aujla <sahibpreet568@gmail.com>
2025-07-01 14:23:45 +0530

    added class for 2d vector

--- billiards/script.js
+++ billiards/script.js
@@ -20,6 +20,55 @@ function cY(pos){
     return canvas.height - pos.y * cScale; 
 }
 
+//define a vector class
+class Vector2{
+    constructor(x,y){
+        this.x = x;
+        this.y = y;
+    }
+
+    add(v,s=1.0){
+        this.x+= v.x * s;
+        this.y+= v.y * s;
+        return this;
+    }
+    addVectors(a,b){
+        this.x= a.x + b.x;
+        this.y= a.y + b.y;
+        return this;
+    }
+
+    subtract(v,s=1.0){
+        this.x-=v.x*s;
+        this.y-=v.y*s;
+        return this;
+    }
+    subtractVectors(a,b){
+        this.x= a.x - b.x;
+        this.y= a.y - b.y;
+        return this;
+    }
+    clone(){
+        return new Vector2(this.x, this.y);
+    }
+    scale(s){
+        this.x *= s;
+        this.y *= s;
+        return this;
+    }
+    dot(v){
+        return this.x * v.x + this.y * v.y;
+    }
+    length(){
+        return Math.sqrt(this.x * this.x + this.y * this.y);
+    }
+    set(v){
+        this.x = v.x;
+        this.y = v.y;
+        return this;
+    }
+}
+
 
 function draw(){
     ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -29,7 +78,7 @@ function draw(){
 }
 
 function simulate(){
-    
+
 }
 
 
